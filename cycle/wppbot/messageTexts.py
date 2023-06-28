fb1176aa2e0e99ea8e4b0b7ed2af3f96bb161489
ASVdeM <32139391+ASVdeM@users.noreply.github.com>
2023-06-28 18:08:17 -0300

    feat: Adicionar novos textos

--- cycle/wppbot/messageTexts.py
+++ cycle/wppbot/messageTexts.py
@@ -3,22 +3,27 @@ welcome_message_text = \
 """Olá, seja bem-vindo ao serviço de deúncias do CycleSpot :)
 Para fazer uma denúncia, digite 1.
 Para saber mais sobre o Bot do CycleSpot, digite 2.
+Para configurar a frequência das ofertas que você pode receber periodicamente, digite 3.
 """ 
 
 welcome_back_message_text = \
 """Olá de volta ao serviço de deúncias do CycleSpot
 Para fazer uma denúncia, digite 1.
 Para saber mais sobre o Bot do CycleSpot, digite 2.
+Para configurar a frequência das ofertas que você pode receber periodicamente, digite 3.
 """ 
 
-# Escolher categorias para história 1.e (sprint2)
 prompt_category_message_text = \
 """
 Ok, vamos começar. Qual o tipo de problema?
 
 Digite:
 '1' para problemas de Sinalização
-'2' para problemas 
+'2' para problemas de Iluminação
+'3' para problemas de Segurança
+'4' para problemas de Acessibilidade
+'5' para problemas de Manutenção da via
+'6' para problemas que não estejam nas opções acima
 'Sair' para cancelar a denúncia
 """
 
@@ -38,13 +43,43 @@ Digite:
 def prompt_confirm_description(oldDescription):
     return "A descrição da sua denúncia será: \n'" + oldDescription + "'\n" + prompt_confirm_description_options
 
-# Aqui, é legal deixar um link ajudando, talvez
 prompt_location_message_text = \
 """
 Utilize o WhatsApp para enviar a localização de onde encontrou o problema.
+Se você não souber como fazer isto, não tem problema!
+Basta consultar este site aqui: https://canaltech.com.br/apps/como-enviar-sua-localizacao-pelo-whatsapp/
 """
 
 present_reward_message_text = \
 """
 Agradecemos sua denúncia. Fique com a dica de um dos nossos parceiros:\n
+"""
+
+about_text = \
+"""
+O bot do CycleSpot é um bot do Whatsapp que recebe as suas denúncias sobre problemas nas ciclofaixas.
+No futuro, elas serão encaminhadas para a prefeitura para que eles possam melhorar tais problemas.
+Envie o máximo de denúncias que você conseguir, pois assim as ruas irão ficar muito melhor para você e para outros ciclistas também!
+Também, você pode receber ofertas após um certo período de tempo. Isso pode ser configurado.
+Digite 1 para fazer a sua denúncia.
+Digite 3 para configurar a frequência das ofertas que você pode receber periodicamente.
+"""
+
+asleep_text = \
+"""
+Já faz um dia desde a sua última mensagem.
+Digite:
+'1' para continuar.
+'Sair' para cancelar a denúncia.
+"""
+
+choose_frequency = \
+"""
+Digite:
+'1' para receber ofertas a cada 1 dia
+'2' para receber ofertas a cada 2 dias
+'3' para receber ofertas a cada 3 dias
+'4' para receber ofertas a cada 1 semana
+'5' para receber ofertas a cada 2 semanas
+'6' para não receber ofertas
 """
